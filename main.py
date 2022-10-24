13e0cdeadb497e7281aa61d51bf3519fc24f5e80
Ali Muhammad <ali9796408@gmail.com>
2022-10-24 15:21:43 +0500

    dotenv and requirements.txt setup

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import os
 from fastapi import FastAPI
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
 
 
 @app.get("/")
 def home():
-    return {"message": "Hello World!"}
+    return {"message": f"Hello {os.getenv('APP_NAME', 'FastAPI Boilerplate')}"}
